030d80835201ddca480ce5fdb5f9475645013c28
Gustav Mattsén <mattsen03@gmail.com>
2025-09-18 12:02:16 +0200

    Finished AppUserRepository.java

--- src/main/java/org/example/springbootworkshop/repository/AppUserRepository.java
+++ src/main/java/org/example/springbootworkshop/repository/AppUserRepository.java
@@ -1,4 +1,19 @@
 package org.example.springbootworkshop.repository;
 
-public interface AppUserRepository {
+import org.example.springbootworkshop.entity.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
+    Optional<AppUser> findByUsername(String username);
+
+    List<AppUser> findByRegistrationDateBetween(LocalDate startDate, LocalDate endDate);
+
+    Optional<AppUser> findByDetailsId(Long detailsId);
+
+    Optional<AppUser> findByDetailsEmailIgnoreCase(String email);
 }
