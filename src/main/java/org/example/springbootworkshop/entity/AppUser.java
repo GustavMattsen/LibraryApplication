7ab4597c1dfb1a74c324b1f86530c919e19be8db
Gustav Mattsén <mattsen03@gmail.com>
2025-09-18 11:25:14 +0200

    Finished AppUser.java and added NoArgs and AllArgsConstructor to both classes

--- src/main/java/org/example/springbootworkshop/entity/AppUser.java
+++ src/main/java/org/example/springbootworkshop/entity/AppUser.java
@@ -1,24 +1,30 @@
 package org.example.springbootworkshop.entity;
 
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import java.time.LocalDate;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Details {
+public class AppUser {
 
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column (nullable = false, length = 100, unique = true)
-    private String email;
+    @Column(unique = true)
+    private String username;
 
-    private String Name;
+    private LocalDate registrationDate;
+
+    @OneToOne
+    @JoinColumn
+    private Details details;
 }
--- src/main/java/org/example/springbootworkshop/entity/Appuser.java
+++
@@ -1,4 +0,0 @@
-package org.example.springbootworkshop.entity;
-
-public class Appuser {
-}
