f1cf9d289f87c9ba28c44addcd071a9361e93172
Gustav Mattsén <mattsen03@gmail.com>
2025-09-18 10:49:47 +0200

    Finished Details.java

--- src/main/java/org/example/springbootworkshop/entity/Details.java
+++ src/main/java/org/example/springbootworkshop/entity/Details.java
@@ -1,4 +1,20 @@
 package org.example.springbootworkshop.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Details {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String email;
+
+    private String Name;
 }
